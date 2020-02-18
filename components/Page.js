9a144624a491f963ca5c881d628d55aff2d9b86f
Kyle Anderson <kmanderson12@gmail.com>
2020-02-18 14:27:00 -0600

    fixed preview box style

--- components/Page.js
+++ components/Page.js
@@ -22,6 +22,7 @@ const theme = {
     red: '#FC8181'
   },
   shadows: {
+    inset: 'inset 0px 2px 4px rgba(0, 0, 0, 0.06)',
     bs1: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
     bs2: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)'
   },
